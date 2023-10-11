d48effa52a48d9bc3d7dbd64cbd32077f0edf903
Valérian LAMBERT <valerian.lambert@brassart.fr>
2023-10-11 09:09:40 +0200

    Premiet jet correspondance couleurs

--- Assets/200_Scripts/ObjectInteraction.cs
+++ Assets/200_Scripts/ObjectInteraction.cs
@@ -6,6 +6,7 @@ public class ObjectInteraction : MonoBehaviour
 {
     public float interactRange = 10f;
     [SerializeField] private Camera fpCam;
+    [SerializeField] private Barrel barrel;
     public static List<GameObject> RedDoors = new List<GameObject>();
     public static List<GameObject> BlueDoors = new List<GameObject>();
 
@@ -14,34 +15,37 @@ public class ObjectInteraction : MonoBehaviour
         if (Input.GetButtonDown("Fire1")) // Le joueur tire
         {
             Ray ray = fpCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            Debug.Log("Shoot"); 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, interactRange))
+            Debug.Log("Shoot");
+            if(barrel.barrelStock.Count > 0)
             {
-                if (hit.collider.CompareTag("BlueButton"))
+                if (Physics.Raycast(ray, out hit, interactRange))
                 {
-                    Debug.Log("BlueButton");
-                    // Le joueur a tiré sur un objet bleu, désactivez les portes bleues et les portes rouges
-                    foreach (GameObject door in RedDoors)
+                    if (barrel.barrelStock[0] == Color.blue && hit.collider.CompareTag("BlueButton"))
                     {
-                        door.SetActive(true);
+                        Debug.Log("BlueButton");
+                        // Le joueur a tiré sur un objet bleu, désactivez les portes bleues et les portes rouges
+                        foreach (GameObject door in RedDoors)
+                        {
+                            door.SetActive(true);
+                        }
+                        foreach (GameObject door in BlueDoors)
+                        {
+                            door.SetActive(false);
+                        }
                     }
-                    foreach (GameObject door in BlueDoors)
+                    else if (barrel.barrelStock[0] == Color.red && hit.collider.CompareTag("RedButton"))
                     {
-                        door.SetActive(false);
-                    }
-                }
-                else if (hit.collider.CompareTag("RedButton"))
-                {
-                    Debug.Log("Redbutton"); 
-                    // Le joueur a tiré sur un objet rouge, désactivez les portes rouges et les portes bleues
-                    foreach (GameObject door in RedDoors)
-                    {
-                        door.SetActive(false);
-                    }
-                    foreach (GameObject door in BlueDoors)
-                    {
-                        door.SetActive(true);
+                        Debug.Log("Redbutton");
+                        // Le joueur a tiré sur un objet rouge, désactivez les portes rouges et les portes bleues
+                        foreach (GameObject door in RedDoors)
+                        {
+                            door.SetActive(false);
+                        }
+                        foreach (GameObject door in BlueDoors)
+                        {
+                            door.SetActive(true);
+                        }
                     }
                 }
             }
