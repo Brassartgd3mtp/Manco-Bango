ba434d7f61f05db955bbcbbf3e6abf85666a700b
Hugo PETRUZZELLI <hugo.petruzzelli@brassart.fr>
2023-10-23 14:10:47 +0200

    Ajout LD / Asset UI

--- Assets/200_Scripts/210_Player/PlayerController.cs
+++ Assets/200_Scripts/210_Player/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 
 public class PlayerController : MonoBehaviour
 {
--- Assets/200_Scripts/250_Enviro/ObjectInteraction.cs
+++ Assets/200_Scripts/250_Enviro/ObjectInteraction.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class ObjectInteraction : MonoBehaviour
