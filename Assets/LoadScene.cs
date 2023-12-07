ea36280721c7e8c6d1133e668808b28c82e41f08
Hugo PETRUZZELLI <hugo.petruzzelli@brassart.fr>
2023-12-07 13:40:34 +0100

    UI Main menu

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScene : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
+
+
+    public void LoadingScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+   
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+
+
+        }
+    }
+
+}
