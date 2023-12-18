89b807462b9cbb909ab83e400250e5939a6f416e
Valerian Lambert <valerian.lambert13@gmail.com>
2023-12-18 11:33:58 +0100

    Bug Retour Menu et Reload Scene

--- Assets/200_Scripts/220_UI/Menu_Script.cs
+++ Assets/200_Scripts/220_UI/Menu_Script.cs
@@ -5,23 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Menu_Script : MonoBehaviour
 {
-    // Nom du niveau que vous souhaitez relancer
-    public string Level_1 = "Level_1";
-    public string Main_Menu = "Main_Menu";
-
-
     // Méthode appelée lorsqu'on appuie sur le bouton
     public void RelancerLeNiveau()
     {
         // Charger à nouveau le même niveau
-        SceneManager.LoadScene(Level_1);
+        SceneManager.LoadScene(1);
     } 
     
     public void RetourMenu()
     {
         // Charger à nouveau le même niveau
-        SceneManager.LoadScene(Main_Menu);
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
     }
-
-
 }
